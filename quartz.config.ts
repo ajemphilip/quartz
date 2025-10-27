a98637ad222ce48311882fb1ef6103e32923a697
Philip <philipn1@my.yorku.ca>
2025-10-27 13:30:23 -0400

    Quartz sync: Oct 27, 2025, 1:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Masters Projects",
+    pageTitle: "Research Projects",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
